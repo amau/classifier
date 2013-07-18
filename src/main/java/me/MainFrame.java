e1383bb881eb615bc94ca8ff4d34d595ad5abc5f
amau <amaury.gtz@gmail.com>
2013-07-17 21:41:10 -0500

    Added a new Panel to handle the images.

--- src/main/java/me/MainFrame.java
+++ src/main/java/me/MainFrame.java
@@ -1,75 +1,180 @@
+
 package me;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import java.io.*;
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
-public class MainFrame
+import me.ui.ClassifierPanel;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.FileFilter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainFrame implements KeyListener
 {
 	protected final JFrame frame = new JFrame("FrameDemo");
+
 	protected final JFileChooser fileChooser = new JFileChooser();
+
 	protected JLabel label = new JLabel("Chose a directory.");
+
+	protected ClassifierPanel panel;
+
 	protected File directory;
+
+	protected File[] files;
+
+	protected List<File> animals = new ArrayList<File>();
+
+	protected List<File> empty = new ArrayList<File>();
+
+	protected int count = 0;
+
 
 	public MainFrame()
 	{
 
 	}
 
-	public void createAndShowGUI() {
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
- 
-        JLabel emptyLabel = new JLabel("");
-        emptyLabel.setPreferredSize(new Dimension(175, 100));
-        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
 
-        JMenuBar menuBar  = new JMenuBar();
-        JMenu    fileMenu = new JMenu("File");
-        JMenuItem openItem = new JMenuItem("Open...");
-        openItem.addActionListener(new ActionListener(){
-        	public void actionPerformed(ActionEvent e) {
-		    	fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		        int retval = fileChooser.showOpenDialog(frame);
-		        if (retval == JFileChooser.APPROVE_OPTION) {
-		            start(fileChooser.getSelectedFile());
-		        }
-	    	}
-        });
-        menuBar.add(fileMenu);
-        fileMenu.add(openItem);
+	public void createAndShowGUI()
+	{
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.setJMenuBar(menuBar);
-        frame.getContentPane().add(label);
-        frame.pack();
-        frame.setVisible(true);
-    }
+		JLabel emptyLabel = new JLabel("");
+		emptyLabel.setPreferredSize(new Dimension(175, 100));
+		frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		JMenuItem openItem = new JMenuItem("Open...");
+		openItem.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				fileChooser
+						.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				int retval = fileChooser.showOpenDialog(frame);
+				if (retval == JFileChooser.APPROVE_OPTION)
+				{
+					start(fileChooser.getSelectedFile());
+				}
+			}
+		});
+		menuBar.add(fileMenu);
+		fileMenu.add(openItem);
+
+		frame.setJMenuBar(menuBar);
+		frame.getContentPane().add(label);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setResizable(false);
+	}
+
+
 	public void start(File directory)
 	{
-		System.out.println("Start Process: " + directory);
-		File[] files = directory.listFiles(new FileFilter(){
-        	public boolean accept(File fileName) {
-            	return fileName.getName().matches(".*\\.jpe?g$") || fileName.getName().matches(".*\\.gif$") || fileName.getName().matches(".*\\.png$");
-        }}); 
-
-
-		
-        for(int i=0 ; i<files.length ; i++)
-        {
-        	try{
-        		ImageIcon imageIcon = new ImageIcon(files[i].getAbsolutePath());
-        		label.setIcon(imageIcon);
-				frame.setSize(imageIcon.getIconWidth(), imageIcon.getIconHeight());
-				frame.update(frame.getGraphics() );
-        		System.out.println("File " + i + ": " + files[i].getName());
-        		Thread.sleep(2000);
+		files = directory.listFiles(new FileFilter()
+		{
+			public boolean accept(File fileName)
+			{
+				return fileName.getName().matches(".*\\.jpe?g$")
+						|| fileName.getName().matches(".*\\.gif$")
+						|| fileName.getName().matches(".*\\.png$");
 			}
-			catch(InterruptedException e) {
-				e.printStackTrace();
-			}
+		});
 
-        }
+		panel = new ClassifierPanel(files[count].getAbsolutePath());
+		frame.remove(label);
+		frame.add(panel);
+		frame.addKeyListener(this);
+		resize();
+	}
+
+
+	public void resize()
+	{
+		Insets i = frame.getInsets();
+		frame.setSize(new Dimension(
+				panel.getWidth() + i.left + i.right, panel.getHeight()
+						+ i.top + i.bottom));
+	}
+
+
+	public void update()
+	{
+		panel.setImage(files[count]);
+		panel.update(panel.getGraphics());
+		resize();
+	}
+
+
+	private void end()
+	{
+		panel.end(panel.getGraphics());
+		System.out.println("");
+		System.out.println("Pictures with animals: ");
+		System.out.println("");
+		for (File f : this.animals)
+		{
+			System.out.println(f.getName());
+		}
+		System.out.println("");
+		System.out.println("Pictures without animals: ");
+		System.out.println("");
+		for (File f : this.empty)
+		{
+			System.out.println(f.getName());
+		}
+		resize();
+	}
+
+
+	public void keyTyped(KeyEvent e)
+	{
+		// We don't want to do anything on key typed.
+	}
+
+
+	public void keyPressed(KeyEvent e)
+	{
+		if (e.getKeyCode() == 37 || e.getKeyCode() == 39)
+		{
+			if (e.getKeyCode() == 37)
+			{
+				animals.add(files[count]);
+			}
+			else
+			{
+				empty.add(files[count]);
+			}
+			count++;
+			if (count < files.length)
+			{
+				update();
+			}
+			else
+			{
+				end();
+			}
+		}
+	}
+
+
+	public void keyReleased(KeyEvent e)
+	{
+		// We don't want to do anything on key released.
 	}
 }
